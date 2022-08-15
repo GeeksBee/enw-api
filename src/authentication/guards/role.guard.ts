817e5f56e93e1cb168823b5260e60e75f0e101cd
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-08-15 16:56:09 +0530

    fix RoleGuard to extend JwtAuthenticationGuard

--- src/authentication/guards/role.guard.ts
+++ src/authentication/guards/role.guard.ts
@@ -1,10 +1,12 @@
 import { UserRole } from "src/modules/user/entities/user.entity";
 import { CanActivate, ExecutionContext, mixin, Type } from "@nestjs/common";
 import RequestWithUser from "../interfaces/requestWithUser.interface";
+import JwtAuthenticationGuard from "./jwtAuthentication.guard";
 
 const RoleGuard = (role: UserRole): Type<CanActivate> => {
-    class RoleGuardMixin implements CanActivate {
-        canActivate(context: ExecutionContext): boolean {
+    class RoleGuardMixin extends JwtAuthenticationGuard {
+        async canActivate(context: ExecutionContext): Promise<boolean> {
+            await super.canActivate(context);
             const request = context.switchToHttp().getRequest<RequestWithUser>();
             const user = request.user;
             return user?.role == role;
