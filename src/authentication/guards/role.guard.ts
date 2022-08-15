74235be4cf0b0c4b2a6287aa4abcb48ad0ceba9a
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-08-15 21:48:29 +0530

    add multiple role to Role Gaurd

--- src/authentication/guards/role.guard.ts
+++ src/authentication/guards/role.guard.ts
@@ -3,13 +3,13 @@ import { CanActivate, ExecutionContext, mixin, Type } from "@nestjs/common";
 import RequestWithUser from "../interfaces/requestWithUser.interface";
 import JwtAuthenticationGuard from "./jwtAuthentication.guard";
 
-const RoleGuard = (role: UserRole): Type<CanActivate> => {
+const RoleGuard = (roles: UserRole[]): Type<CanActivate> => {
     class RoleGuardMixin extends JwtAuthenticationGuard {
         async canActivate(context: ExecutionContext): Promise<boolean> {
             await super.canActivate(context);
             const request = context.switchToHttp().getRequest<RequestWithUser>();
             const user = request.user;
-            return user?.role == role;
+            return roles.filter((role: UserRole) => user.role === role).length !== 0;
         }
     }
     return mixin(RoleGuardMixin);
