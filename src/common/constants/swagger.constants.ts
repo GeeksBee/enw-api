9ce82f6ec42d3621eb34f62528ee573346103459
rahulJalan23 <rahuljalan23.rj@gmail.com>
2022-08-21 17:50:02 +0530

    jobs basic routes

--- src/common/constants/swagger.constants.ts
+++ src/common/constants/swagger.constants.ts
@@ -2,4 +2,5 @@ export const apiTags = {
     Authentication: "Authentication",
     Payment: "Payment",
     Organisation: "Organisation",
+    Job: "Job",
 };
--- src/modules/job/dto/create-job.dto.ts
+++ src/modules/job/dto/create-job.dto.ts
@@ -1,1 +1,37 @@
-export class CreateJobDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateJobDto {
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    organisationId: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    minAge: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    maxAge: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    yearsOfExperience: number;
+
+    @IsNotEmpty()
+    salaryRange: {
+        start: number;
+        end: number;
+    };
+
+    @IsString()
+    @IsNotEmpty()
+    pincode: string;
+}
