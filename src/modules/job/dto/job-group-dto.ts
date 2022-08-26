0b3fce5ba87bb9ee6c0e47606d93a477359ddaf4
rahulJalan23 <rahuljalan23.rj@gmail.com>
2022-08-26 16:23:12 +0530

    minor update

--- src/modules/job/dto/job-group-dto.ts
+++ src/modules/job/dto/job-group-dto.ts
@@ -1,8 +1,17 @@
 // import { IsNotEmpty, IsString } from "class-validator";
 
+import { ApiProperty } from "@nestjs/swagger";
+
 export class JobGroupDto {
+    @ApiProperty()
     title: string;
+
+    @ApiProperty()
     description: string;
+
+    @ApiProperty()
     applicationLink: string;
+
+    @ApiProperty()
     path: string;
 }
--- src/modules/job/job.service.ts
+++ src/modules/job/job.service.ts
@@ -210,7 +210,7 @@ export class JobService {
     async findIncompleteJobGroups(): Promise<JobGroup[]> {
         const jobGroups = await this.jobGroupRepo.find({
             where: {
-                isComplete: false,
+                completed: false,
             },
         });
         return jobGroups;
